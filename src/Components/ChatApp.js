42b8e831dd20dcc746ac629983df0e66b7c77845
Ophir Huberman <77234895+hubermanophir@users.noreply.github.com>
2021-05-14 20:00:07 +0300

    Added dialog create

--- src/Components/ChatApp.js
+++ src/Components/ChatApp.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import firebase from 'firebase';
+import { v4 as uuidv4 } from 'uuid';
+import FormDialog from './FormDialog';
 
 export default function ChatApp() {
+	const addHandler = () => {};
 	return (
 		<div>
+			<FormDialog />
+			<button onClick={addHandler}>Add chatroom</button>
 			<button onClick={() => firebase.auth().signOut()}>Sign Out</button>
 		</div>
 	);
